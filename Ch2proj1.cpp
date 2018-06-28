e8c384f68cf396a7f0bdb1b55ff7bda63eb02d70
BoilerMakerStephen <sjsctt2@hotmail.com>
2018-06-28 10:50:16 -0600

    Add files via upload

---
+++ Ch2proj1.cpp
@@ -0,0 +1,37 @@
+//Problem Solving With C++ - Walter Savitch
+//Chapter 2
+//Programming Project #1
+//This program reads the weight of a package of breakfast cereal in ounces and 
+//and outputs the weight in metrics tons. The program also calculates the amount
+//of boxes required to contain 1 metric ton per box.
+
+#include <iostream>
+#include <conio.h>
+
+const double metric_ton = 35273.92; //in ounces
+
+using namespace::std;
+
+int main()
+{
+	double oats, fin_amt, box;
+	char ans;
+
+	do{
+	cout << "Enter the weight of a box of cereal oats in ounces you wish"
+		 << " to convert to metric tons:\n";
+	cin >> oats;
+
+	fin_amt = oats/metric_ton;
+
+	cout << oats << " ounces of oats converts to " << fin_amt << " metric tons of cereal oats.\n";
+
+	box = metric_ton/oats;
+
+	cout << "It would take " << box << " boxes of cereal oats to yield one metric ton.\n";
+
+	cout << "Do you wish to perform another calculation?\n";
+	cin >> ans;
+	}while(ans == 'Y' || ans == 'y');
+}
+
